08079b9b4bc3abcfcea6c7aa1039c5f4eaa158fb
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-17 21:22:53 +0200

    add SpliteratorResultConverter tests

--- src/test/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/result/ResultConverterTest.groovy
+++ src/test/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/result/ResultConverterTest.groovy
@@ -11,6 +11,23 @@ import spock.lang.Specification
 class ResultConverterTest extends Specification {
     final conv = new ResultConverter()
 
+    def 'toStrings the first element of a Spliterator into a TextResult'() {
+        given:
+        final obj = new Object()
+        final res = conv(Spliterators.spliterator([obj], 0)) as TextResult
+
+        expect:
+        res.text == obj.toString()
+    }
+
+    def 'converts an empty Spliterator to null toStringed into a TextResult'() {
+        given:
+        final res = conv(Spliterators.emptySpliterator()) as TextResult
+
+        expect:
+        res.text == 'null'
+    }
+
     def 'wraps a Runnable in an InvokeActionResult'() {
         given:
         final run = { -> } as Runnable
