75d7be69bee16a9ac5da57216a8d970f9c3f25d1
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-17 15:20:07 +0200

    add ResultConverter

---
+++ src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/result/ResultConverter.groovy
@@ -0,0 +1,7 @@
+package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.result
+/**
+ * @author seckinger
+ * @since 10/17/16
+ */
+class ResultConverter extends DefaultResultConverter {
+}
