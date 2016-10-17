b90f4951d4f80a0d04510bd5aa56c179b3a4dfd8
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-17 21:53:43 +0200

    add BaseStreamConverter to ResultConverter

--- src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/result/ResultConverter.groovy
+++ src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/result/ResultConverter.groovy
@@ -1,9 +1,13 @@
 package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.result
+
+import com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.generic.BaseStreamConverter
+
 /**
  * @author seckinger
  * @since 10/17/16
  */
 class ResultConverter extends DefaultResultConverter implements
+        BaseStreamConverter,
         RunnableInvokeActionResultConverter,
         SpliteratorResultConverter {
 }
