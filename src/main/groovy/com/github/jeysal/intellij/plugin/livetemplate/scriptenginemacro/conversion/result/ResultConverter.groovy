92527aab2daf37d11b1bc443610de01f6d1e1ac2
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-17 21:00:18 +0200

    add RunnableInvokeActionResultConverter

--- src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/result/ResultConverter.groovy
+++ src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/result/ResultConverter.groovy
@@ -3,5 +3,5 @@ package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.convers
  * @author seckinger
  * @since 10/17/16
  */
-class ResultConverter extends DefaultResultConverter {
+class ResultConverter extends DefaultResultConverter implements RunnableInvokeActionResultConverter {
 }
---
+++ src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/result/RunnableInvokeActionResultConverter.groovy
@@ -0,0 +1,18 @@
+package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.result
+
+import com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.Converter
+import com.intellij.codeInsight.template.InvokeActionResult
+import com.intellij.codeInsight.template.Result
+
+/**
+ * @author seckinger
+ * @since 10/17/16
+ */
+trait RunnableInvokeActionResultConverter implements Converter<Object, Result> {
+    @Override
+    Result call(Object obj) {
+        obj instanceof Runnable ?
+                new InvokeActionResult(obj) :
+                super.call(obj)
+    }
+}
