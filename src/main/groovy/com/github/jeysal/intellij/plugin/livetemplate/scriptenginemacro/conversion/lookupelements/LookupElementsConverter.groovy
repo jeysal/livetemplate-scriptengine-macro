77f894d8ac52f4ef71ccec303bd0097a5dfdb3ce
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-22 16:57:29 +0200

    add LookupElementsConverter

---
+++ src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/lookupelements/LookupElementsConverter.groovy
@@ -0,0 +1,8 @@
+package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.lookupelements
+
+/**
+ * @author seckinger
+ * @since 10/18/16
+ */
+class LookupElementsConverter extends DefaultLookupElementsConverter {
+}
