af354d790e3a668c7cf3ef50a24d9f59df8e2a07
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-23 12:49:37 +0200

    add BaseSteamConverter to LookupElementsConverter

--- src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/lookupelements/LookupElementsConverter.groovy
+++ src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/lookupelements/LookupElementsConverter.groovy
@@ -1,9 +1,12 @@
 package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.lookupelements
 
+import com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.generic.BaseStreamConverter
+
 /**
  * @author seckinger
  * @since 10/18/16
  */
 class LookupElementsConverter extends DefaultLookupElementsConverter implements
+        BaseStreamConverter,
         SpliteratorLookupElementsConverter {
 }
