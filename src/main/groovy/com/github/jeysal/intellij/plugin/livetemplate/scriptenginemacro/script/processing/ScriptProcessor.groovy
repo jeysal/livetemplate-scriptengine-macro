5149a90c90299b6208176d1367da863efc1cbdf4
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-24 20:46:55 +0200

    add ScriptProcessor

---
+++ src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/script/processing/ScriptProcessor.groovy
@@ -0,0 +1,8 @@
+package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.script.processing
+
+/**
+ * @author seckinger
+ * @since 10/24/16
+ */
+class ScriptProcessor extends DefaultScriptProcessor {
+}
