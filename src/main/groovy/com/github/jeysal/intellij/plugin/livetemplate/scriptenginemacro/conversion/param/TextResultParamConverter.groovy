5e62ee6d60eb6078096c5f45f888d778cbc22e3f
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-24 18:06:00 +0200

    add TextResultParamConverter

---
+++ src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/param/TextResultParamConverter.groovy
@@ -0,0 +1,17 @@
+package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.param
+
+import com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.Converter
+import com.intellij.codeInsight.template.Result
+import com.intellij.codeInsight.template.TextResult
+
+/**
+ * @author seckinger
+ * @since 10/24/16
+ */
+trait TextResultParamConverter implements Converter<Result, Object> {
+    Object call(Result res) {
+        res instanceof TextResult ?
+                res.text :
+                super.call(res)
+    }
+}
