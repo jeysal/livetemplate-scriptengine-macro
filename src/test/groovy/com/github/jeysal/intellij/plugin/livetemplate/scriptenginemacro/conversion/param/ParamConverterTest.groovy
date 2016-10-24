37661d7dc63c4fd1ef9c093a03ee25043ccd90ac
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-24 18:02:52 +0200

    add InvokeActionResultParamConverter test

--- src/test/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/param/ParamConverterTest.groovy
+++ src/test/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/param/ParamConverterTest.groovy
@@ -1,5 +1,6 @@
 package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.param
 
+import com.intellij.codeInsight.template.InvokeActionResult
 import com.intellij.codeInsight.template.Result
 import spock.lang.Specification
 
@@ -9,6 +10,14 @@ import spock.lang.Specification
  */
 class ParamConverterTest extends Specification {
     def conv = new ParamConverter()
+
+    def 'unwraps an InvokeActionsResult'() {
+        given:
+        Runnable noop = { -> }
+
+        expect:
+        conv.call(new InvokeActionResult(noop)) == noop
+    }
 
     def 'toStrings any Result'() {
         given:
