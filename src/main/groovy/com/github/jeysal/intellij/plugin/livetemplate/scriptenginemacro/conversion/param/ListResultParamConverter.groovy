400f5f02d4231e28e235303de6f98c00e5285f8a
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-24 18:12:40 +0200

    add ListResultParamConverter

---
+++ src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/param/ListResultParamConverter.groovy
@@ -0,0 +1,17 @@
+package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.param
+
+import com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.Converter
+import com.intellij.codeInsight.template.ListResult
+import com.intellij.codeInsight.template.Result
+
+/**
+ * @author seckinger
+ * @since 10/24/16
+ */
+trait ListResultParamConverter implements Converter<Result, Object> {
+    Object call(Result res) {
+        res instanceof ListResult ?
+                res.components.collect(this.&call) :
+                super.call(res)
+    }
+}
