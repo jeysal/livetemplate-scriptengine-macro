135145edca8b7bc5fab92f6d6cb1053b9df605f0
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-24 18:11:51 +0200

    add ListResultParamConverter tests

--- src/test/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/param/ParamConverterTest.groovy
+++ src/test/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/param/ParamConverterTest.groovy
@@ -1,6 +1,7 @@
 package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.param
 
 import com.intellij.codeInsight.template.InvokeActionResult
+import com.intellij.codeInsight.template.ListResult
 import com.intellij.codeInsight.template.Result
 import com.intellij.codeInsight.template.TextResult
 import spock.lang.Specification
@@ -11,6 +12,24 @@ import spock.lang.Specification
  */
 class ParamConverterTest extends Specification {
     def conv = new ParamConverter()
+
+    def 'converts each element of a ListResult into a List'() {
+        given:
+        Runnable noop = { -> }
+        def res = Mock(Result)
+
+        expect:
+        conv.call(new ListResult([
+                new TextResult('asdf'),
+                new InvokeActionResult(noop),
+                res
+        ])) == ['asdf', noop, res.toString()]
+    }
+
+    def 'converts an empty ListResult to an empty List'() {
+        expect:
+        conv.call(new ListResult([])) == []
+    }
 
     def 'unwraps a TextResult'() {
         expect:
