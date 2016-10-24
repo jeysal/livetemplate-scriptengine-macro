94510d6b3a0cedda0f452aa1cc0a1f76304dd7c1
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-24 17:58:31 +0200

    add ParamConverter

---
+++ src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/conversion/param/ParamConverter.groovy
@@ -0,0 +1,8 @@
+package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.param
+
+/**
+ * @author seckinger
+ * @since 10/24/16
+ */
+class ParamConverter extends DefaultParamConverter {
+}
