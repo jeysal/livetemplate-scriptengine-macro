2d6491d007f9dc103a7586bfa87c8c5d294140ed
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-25 10:54:05 +0200

    move ScriptProcessor test to correct package

--- src/test/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/script/processing/ScriptProcessorTest.groovy
+++ src/test/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/script/processing/ScriptProcessorTest.groovy
@@ -1,7 +1,6 @@
-package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.conversion.script.processing
+package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.script.processing
 
 import com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.execution.data.Script
-import com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.script.processing.ScriptProcessor
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
