f558a34e665561ce2886caf0d25a69600adc62b3
Tim Seckinger <seckinger.tim@gmail.com>
2016-10-25 10:40:51 +0200

    add FileScriptProcessor

---
+++ src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/script/processing/FileScriptProcessor.groovy
@@ -0,0 +1,31 @@
+package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.script.processing
+
+import com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.execution.data.Script
+import org.apache.commons.io.FilenameUtils
+
+import javax.script.ScriptEngineManager
+
+/**
+ * @author seckinger
+ * @since 10/24/16
+ */
+trait FileScriptProcessor {
+    private static final BASE_DIRS = ['', System.getProperty('user.home')]
+    private final manager = new ScriptEngineManager()
+
+    Script apply(final obj, final String lang) {
+        if (obj instanceof CharSequence) {
+            final param = obj.toString()
+            final file = BASE_DIRS.collect { new File(it, param) }.find { it.exists() }
+
+            if (file) {
+                final newLang = lang ?:
+                        manager.getEngineByExtension(FilenameUtils.getExtension(file.name))?.factory?.languageName
+
+                if (newLang)
+                    return new Script(newLang, file.text)
+            }
+        }
+        super.call(obj, lang)
+    }
+}
