c2ee2bd2a640e92feab532a8c414533349f1f132
Tim Seckinger <seckinger.tim@gmail.com>
2016-11-29 16:23:32 +0100

    implement Executor

--- src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/execution/Executor.groovy
+++ src/main/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/execution/Executor.groovy
@@ -3,6 +3,7 @@ package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.executi
 import com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.execution.data.Execution
 
 import javax.script.ScriptEngineManager
+import javax.script.ScriptException
 import java.util.function.Function
 
 /**
@@ -14,6 +15,37 @@ class Executor implements Function<Execution, Object> {
 
     @Override
     Object apply(Execution execution) {
-        null
+        final ctx = execution.context
+        final script = execution.script
+
+        // find engine
+
+        final lang = script.language
+        final engine = manager.engineFactories.find {
+            it.languageName == lang
+        }.scriptEngine
+
+        if (!engine)
+            throw new RuntimeException("Failed to find ScriptEngine for language $lang")
+
+        // script vars
+
+        engine.put '_args', ctx.args
+        engine.put '_goal', ctx.goal.toString()
+        engine.put '_editor', ctx.editor
+
+        // output
+
+        final out = new StringWriter()
+
+        engine.context.writer = out
+        engine.put '_out', out
+
+        // run it
+        try {
+            return engine.eval(script.source)
+        } catch (ScriptException e) {
+            return e
+        }
     }
 }
