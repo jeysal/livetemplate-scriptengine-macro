58252d32129c82dac4a4de957610a9d3cc176e53
Tim Seckinger <seckinger.tim@gmail.com>
2016-11-29 18:50:50 +0100

    add simple string GroovyIntegrationTest

---
+++ src/test/groovy/com/github/jeysal/intellij/plugin/livetemplate/scriptenginemacro/integration/GroovyIntegrationTest.groovy
@@ -0,0 +1,52 @@
+package com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.integration
+
+import com.github.jeysal.intellij.plugin.livetemplate.scriptenginemacro.ScriptEngineMacro
+import com.intellij.codeInsight.template.Expression
+import com.intellij.codeInsight.template.ExpressionContext
+import com.intellij.codeInsight.template.TextResult
+import spock.lang.Specification
+
+import javax.script.ScriptEngineManager
+
+import static org.junit.Assume.assumeNotNull
+
+/**
+ * @author seckinger
+ * @since 11/29/16
+ */
+class GroovyIntegrationTest extends Specification {
+    final macro = new ScriptEngineMacro()
+    final Expression scriptParam = Mock()
+    final ExpressionContext ctx = Mock()
+
+    def setupSpec() {
+        assumeNotNull new ScriptEngineManager().getEngineByName('groovy')
+    }
+
+    private src(String src) {
+        scriptParam.calculateResult(ctx) >> new TextResult("groovy:$src")
+    }
+
+    private expr(String text) {
+        Mock(Expression) {
+            calculateResult(ctx) >> new TextResult(text)
+        }
+    }
+
+    def 'script returns a string'() {
+        given:
+        src($/"asdf"/$)
+
+        when:
+        TextResult res = macro.calculateResult([scriptParam] as Expression[], ctx)
+
+        then:
+        res.text == 'asdf'
+
+        when:
+        List elems = macro.calculateLookupItems([scriptParam] as Expression[], ctx).collect { it.lookupString }
+
+        then:
+        elems == ['asdf']
+    }
+}
